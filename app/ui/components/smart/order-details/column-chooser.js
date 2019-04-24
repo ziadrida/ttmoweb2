0eed52ca81e43392ae0895f9e0f2730414ad28df
Ziad Rida <ziad.i.rida@gmail.com>
2019-04-24 14:35:25 +0300

    fix scrapping

--- app/ui/components/smart/order-details/column-chooser.js
+++ app/ui/components/smart/order-details/column-chooser.js
@@ -7,7 +7,6 @@ import FormControl from '@material-ui/core/FormControl';
 import FormGroup from '@material-ui/core/FormGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import { withStyles } from "@material-ui/core/styles";
-//import 'font-awesome/css/font-awesome.min.css';
 import AlarmIcon from '@material-ui/icons/Reorder';
 import  classes from "classnames";
 import Tooltip from '@material-ui/core/Tooltip';
